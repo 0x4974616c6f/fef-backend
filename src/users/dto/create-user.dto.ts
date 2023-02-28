4530e6915922f1f398517ecca55e9ef929d38a6b
ItaloCobains <italobrandao.contato@gmail.com>
2023-02-27 22:44:09 -0300

    feat: add validations

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,24 @@
-export class CreateUserDto {}
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsStrongPassword()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsBoolean()
+  admin: boolean;
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,10 @@
-export class User {}
+import { User } from '@prisma/client';
+
+export class UserEntity implements User {
+  password: string;
+  id: number;
+  email: string;
+  name: string;
+  admin: boolean;
+  createdAt: Date;
+}
